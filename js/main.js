2b20f4bcfd81898b9d4df9a7c53413e1274bb940
SeongJun KIM <sjkymy@gmail.com>
2023-01-09 02:37:43 +0900

    모달창 open, close 기능 구현, 제출버튼 클릭했을 때 input check 개수에 따라 console 창 다르게 표시

--- js/main.js
+++ js/main.js
@@ -6,8 +6,25 @@ const modalClose = () => {
     document.querySelector(".modal").classList.add("hidden");
 }
 
-const modalOpenBtn = document.querySelector(".btn_start");
+const modalOpenBtn = document.querySelector("#btnStart");
 modalOpenBtn.addEventListener("click", modalOpen);
 
-const modalCloseBtn = document.querySelector(".btn_close");
-modalCloseBtn.addEventListener("click", modalClose)
+const modalCloseBtn = document.querySelector("#btnClose");
+modalCloseBtn.addEventListener("click", modalClose);
+
+
+const modalBox = document.querySelector('.modalBox');
+const btnSubmit = modalBox.querySelector('#btnSubmit');
+
+btnSubmit.addEventListener('click', (e) => {
+    const Inputs = 'input[name="riskCheck"]:checked';
+    const result = modalBox.querySelectorAll(Inputs);
+    e.preventDefault()
+
+    if (result.length >= 1) {
+        confirm('지금 운동 못해요')
+    } else {
+        alert('넘어갑시다.')
+        console.log("해당사항 없음.");
+    }
+})
