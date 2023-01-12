62c94dca040a4c8ceb1f822d7393187a4c2dc29d
SeongJun KIM <sjkymy@gmail.com>
2023-01-12 22:21:35 +0900

    인풋 증감 화살표 없애기, 결과값 소수점 첫 째자리까지만 표시

--- fitness_test/fittest.js
+++ fitness_test/fittest.js
@@ -19,14 +19,12 @@ inpTwelve.addEventListener("input", (e) => {
 // 숫자 5자리까지만 입력 가능한 함수
 function handleOnInput(e, maxlength) {
     if(e.value.length > maxlength)  {
-      e.value 
-        = e.value.substr(0, maxlength);
+      e.value = e.value.substr(0, maxlength);
     }
 };
 
 // 버튼 클릭하면 VO2max input에 계산된 결과 전달
 btnSubmit.addEventListener("click", (e) => {
     e.preventDefault()
-    voMax.value = (inpTwelve.value - 504.9)/44.73
-    console.log(typeof(voMax.value));
+    voMax.value = ((inpTwelve.value - 504.9)/44.73).toFixed(1);
 })
