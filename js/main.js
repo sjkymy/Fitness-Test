ea1e9252f6a5ebf019985b44a5340f1f2d77883f
SeongJun KIM <sjkymy@gmail.com>
2023-01-30 19:15:17 +0900

    12분 달리기 section 구현

--- js/main.js
+++ js/main.js
@@ -2,26 +2,49 @@ const toggleBtn = document.querySelector(".btn_toggle");
 const navMenu = document.querySelector(".menu_list");
 const navIcon = document.querySelector(".link_icon");
 const topBar = document.querySelector(".topbar");
+const Main = document.querySelector(".container");
+const twelveTitle = Main.querySelector(".twelve_title");
+const twelveIntro = Main.querySelector(".twelve_intro")
+const twelveDesc = Main. querySelector(".twelve_desc");
 
+// 스크롤 내리면 상단바 배경색 변경
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
+    // console.log(value);
     if (value >= 100) {
         topBar.style = "background-color: #333333";
     } else {
         topBar.style = "background-color: none";
-    }
+    };
+
+    if (value <= 1519) {
+        twelveTitle.style.animation = "disappear 1.5s ease-out forwards";
+        twelveIntro.style.animation = "disappear 1.5s ease-out forwards";
+    } else {
+        twelveTitle.style.animation = "slide 1.5s ease-out";
+        twelveIntro.style.animation = "slide 1.5s ease-out";
+    };
+
+    if (value <= 1750) {
+        twelveDesc.style.animation = "disappear 1.5s ease-out forwards";
+    } else {
+        twelveDesc.style.animation = "slide 1.5s ease-out";
+    };
 })
 
+// 모바일 화면에서 메뉴 버튼 클릭하면 콘텐츠 나타나기
 toggleBtn.addEventListener("click", (e) => {
     e.preventDefault()
     navMenu.classList.toggle("active");
     navIcon.classList.toggle("active");
+    topBar.style = "background-color: #333333"
 })
 
+// 운동 전 설문지 모달창
 const modalOpen = () => {
     document.querySelector(".modal").classList.remove("hidden");
     // 모달창 뒷 배경 스크롤 막기
-    document.body.style= `overflow: hidden`;
+    document.body.style = `overflow: hidden`;
 }
 
 const modalClose = () => {
@@ -34,7 +57,6 @@ modalOpenBtn.addEventListener("click", modalOpen);
 
 const modalCloseBtn = document.querySelector("#btnClose");
 modalCloseBtn.addEventListener("click", modalClose);
-
 
 const modalBox = document.querySelector(".modalBox");
 const btnSubmit = modalBox.querySelector("#btnSubmit");
@@ -51,6 +73,7 @@ btnSubmit.addEventListener("click", (e) => {
     }
 });
 
+// 텍스트 애니메이션
 const txtEffect = new IntersectionObserver((e) => {
     if (e[0].isIntersecting) {
         e[0].target.style.opacity = 1;
@@ -63,3 +86,4 @@ const txtEffect = new IntersectionObserver((e) => {
 
 const txtGroup = document.querySelector(".txt_group");
 txtEffect.observe(txtGroup)
+
