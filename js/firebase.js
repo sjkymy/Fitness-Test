7b0c8214815a475564768ac35efd3b01d4721f3e
SeongJun KIM <sjkymy@gmail.com>
2023-02-06 00:18:40 +0900

    firebase data 추가

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +14,7 @@ appId: "1:46431411311:web:164b796dddaf97d8d3c19a"
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const formInfo = document.querySelector(".form_info");
 const Gender = formInfo.querySelector(".gender_select");
@@ -21,9 +22,16 @@ const Age = formInfo.querySelector(".age_select");
 const Email = formInfo.querySelector(".inp_email");
 const btnContact = formInfo.querySelector(".btn_contact");
 
-formInfo.addEventListener("submit", (e) => {
+formInfo.addEventListener("submit", async(e) => {
     e.preventDefault();
-    console.log(Email.value)
+    try {
+        const docRef = await addDoc(collection(db, "waitings"), {
+          Gender: `${Gender.value}`,
+          Age: `${Age.value}`,
+          Email: `${Email.value}`
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (error) {
+        console.error("Error adding document: ", error);
+      }
 })
-
-const db = getFirestore(app);
